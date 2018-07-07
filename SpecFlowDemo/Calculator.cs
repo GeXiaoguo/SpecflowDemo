13f662251c8c0a5ec7af0ecbb8e129ead6b29d70
Xiaoguo Ge <xiaoguo.ge@adslot.com>
2018-07-07 20:01:05 +1000

    initial commit

---
+++ SpecFlowDemo/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowDemo
+{
+    public class Calculator
+    {
+        public int SecondNumber { get; set; }
+        public int FirstNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
---
+++ SpecFlowTests/SpecFlowFeature1Steps.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using SpecFlowDemo;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTests
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private int _result;
+        private Calculator _calculator = new Calculator();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            _calculator.FirstNumber = p0;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int p0)
+        {
+            _calculator.SecondNumber = p0;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            _result = _calculator.Add();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _result);
+        }
+    }
+}
